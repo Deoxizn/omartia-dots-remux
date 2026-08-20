906fa6b57b237ee5beee4c3b80e9e126047a6fbd
Devi Garcia <asphyxiated.god@gmail.com>
2026-08-19 22:39:12 -0700

    Add qt6-shadertools to dependencies (fixes cmake build)

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ fi
 info "Checking dependencies..."
 
 DEPS_PKGS=()
-for pkg in cmake ninja qt6-base qt6-declarative qt6-svg; do
+for pkg in cmake ninja qt6-base qt6-declarative qt6-svg qt6-shadertools; do
   if ! pacman -Qi "$pkg" &>/dev/null; then
     DEPS_PKGS+=("$pkg")
   fi
